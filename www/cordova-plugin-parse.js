9586262fe3bfd0a53eed3df29b74881e1ec616a5
Ivan Fabijanovic <feb@shoutem.com>
2015-09-09 12:37:06 +0200

    Added stuff.

--- www/cordova-plugin-parse.js
+++ www/cordova-plugin-parse.js
@@ -1,5 +1,12 @@
 var exec = require('cordova/exec');
 
+exports.notificationType = {
+    none: 0,
+    badge: 1,
+    sound: 2,
+    alert: 4
+};
+
 exports.initialize = function(applicationId, clientKey, success, error) {
     exec(success, error, "cordova-plugin-parse", "initialize", [applicationId, clientKey]);
 };
